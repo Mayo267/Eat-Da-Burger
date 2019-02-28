2c782cce7767154948398a24db6aa221ba53e23a
Andrew Mayo <amayo0925@gmail.com>
2019-02-28 16:48:25 -0500

    almost finish

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Breakfast Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Salmon Burger', true);
+INSERT INTO burgers (name) VALUES ('Rice Burger');
